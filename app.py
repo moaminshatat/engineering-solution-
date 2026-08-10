79b1f137334d8612d56206a79fd5d65e7b61d9b9
moaminshatat <moaminshatat@gmail.com>
2026-08-10 20:33:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
@@ -7,6 +7,7 @@ def home():
     return """
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 <meta name="google-site-verification" content="Oem5iuWD0GKJOIvOnkEaS8W6Ar3YDHxgALnrojAm0HM" />
 <meta charset="UTF-8">
@@ -131,6 +132,7 @@ padding:20px;
 </section>
 
 <section class="section">
+
 <h2>About Me</h2>
 
 <div class="about">
@@ -140,14 +142,11 @@ Welcome to Momin Engineering Solutions.
 </p>
 
 <p>
-I am Momin Azmi Shatat, a Civil Engineer and MSc student in
-Sustainable Land Management at Vrije Universiteit Brussel.
+I am Momin Azmi Shatat, a Civil Engineer and MSc student in Sustainable Land Management at Vrije Universiteit Brussel.
 </p>
 
 <p>
-My experience includes infrastructure projects, excavation works,
-utility installation, asphalt paving coordination, site supervision,
-and engineering education.
+My experience includes infrastructure projects, excavation works, utility installation, asphalt paving coordination, site supervision, and engineering education.
 </p>
 
 </div>
@@ -216,6 +215,18 @@ and engineering education.
 </body>
 </html>
 """
+
+@app.route("/sitemap.xml")
+def sitemap():
+    xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <url>
+      <loc>https://engineering-solution.onrender.com/</loc>
+      <priority>1.0</priority>
+   </url>
+</urlset>
+"""
+    return Response(xml, mimetype="application/xml")
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
